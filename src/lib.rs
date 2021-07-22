48c3d5df17a638d6179722ce6c26ee35bc2835cf
Markus Reiter <me@reitermark.us>
2021-07-22 09:23:27 +0200

    Add more structs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use serde_repr::*;
+
 pub const HEADER_USER_AGENT: &'static str = "iAqualink/98 CFNetwork/978.0.7 Darwin/18.6.0";
 pub const HEADER_ACCEPT: &'static str = "*/*";
 pub const HEADER_ACCEPT_LANGUAGE: &'static str = "en-us";
@@ -99,19 +101,251 @@ pub struct System {
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
+pub struct Durations {
+  custom_tim: usize,
+  deep_tim: usize,
+  first_smart_tim: usize,
+  quick_tim: usize,
+  smart_tim: usize,
+  water_tim: usize,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct RobotSensor {
+  #[serde(rename = "type")]
+  sensor_type: String,
+  state: usize,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct RobotSensors {
+  sns_1: RobotSensor,
+  sns_2: RobotSensor,
+  sns_3: RobotSensor,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Robot {
+  canister: usize,
+  custom_cyc: usize,
+  cycle_start_time: usize,
+  durations: Durations,
+  equipment_id: String,
+  error_code: usize,
+  error_state: usize,
+  first_smrt_flag: usize,
+  lift_control: usize,
+  logger: usize,
+  pr_cyc: usize,
+  repeat: usize,
+  #[serde(rename = "rmt_ctrl")]
+  rmt_ctrl: usize,
+  scan_time_duration: usize,
+  sch_conf_0_enable: usize,
+  sch_conf_0_hour: usize,
+  sch_conf_0_min: usize,
+  sch_conf_0_prt: usize,
+  #[serde(rename = "schConf0WDay")]
+  sch_conf_0_wday: usize,
+  sch_conf_1_enable: usize,
+  sch_conf_1_hour: usize,
+  sch_conf_1_min: usize,
+  sch_conf_1_prt: usize,
+  #[serde(rename = "schConf1WDay")]
+  sch_conf_1_wday: usize,
+  sch_conf_2_enable: usize,
+  sch_conf_2_hour: usize,
+  sch_conf_2_min: usize,
+  sch_conf_2_prt: usize,
+  #[serde(rename = "schConf2WDay")]
+  sch_conf_2_wday: usize,
+  sch_conf_3_enable: usize,
+  sch_conf_3_hour: usize,
+  sch_conf_3_min: usize,
+  sch_conf_3_prt: usize,
+  #[serde(rename = "schConf3WDay")]
+  sch_conf_3_wday: usize,
+  sch_conf_4_enable: usize,
+  sch_conf_4_hour: usize,
+  sch_conf_4_min: usize,
+  sch_conf_4_prt: usize,
+  #[serde(rename = "schConf4WDay")]
+  sch_conf_4_wday: usize,
+  sch_conf_5_enable: usize,
+  sch_conf_5_hour: usize,
+  sch_conf_5_min: usize,
+  sch_conf_5_prt: usize,
+  #[serde(rename = "schConf5WDay")]
+  sch_conf_5_wday: usize,
+  sch_conf_6_enable: usize,
+  sch_conf_6_hour: usize,
+  sch_conf_6_min: usize,
+  sch_conf_6_prt: usize,
+  #[serde(rename = "schConf6WDay")]
+  sch_conf_6_wday: usize,
+  sensors: RobotSensors,
+  state: usize,
+  stepper: usize,
+  stepper_adj_time: usize,
+  total_hours: usize,
+  vr: String,
+  custom_intensity: usize,
+  #[serde(flatten)]
+  _unknown_fields: HashMap<String, serde_json::Value>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct Sensor {
+  sensor_type: String,
+  state: usize,
+  value: usize,
+}
+
+#[derive(Debug, Serialize_repr, Deserialize_repr)]
+#[repr(u8)]
+pub enum Color {
+  AlpineWhite = 0,
+  SkyBlue = 1,
+  CobaltBlue = 2,
+  CarribeanBlue = 3,
+  SpringGreen = 4,
+  EmeraldGreen = 5,
+  EmeraldRose = 6,
+  Magenta = 7,
+  Violet = 8,
+  SlowFade = 9,
+  FastFade = 10,
+  BeautifulAmerica = 11,
+  CarnivalTuesday = 12,
+  DiscoStyle = 13,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct Auxiliary {
+  #[serde(rename = "type")]
+  aux_type: String,
+  color: Color,
+  mode: usize,
+  state: usize,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FilterPump {
+  #[serde(rename = "type")]
+  pump_type: usize,
+  state: usize,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct SaltWaterChlorinator {
+  amp: usize,
+  temp: usize,
+  vr: String,
+  sns_3: Sensor,
+  orp_sp: usize,
+  production: usize,
+  boost: usize,
+  ph_sp: usize,
+  swc: usize,
+  version: String,
+  sns_2: Sensor,
+  low: usize,
+  vsp: usize,
+  lang: usize,
+  ph_only: usize,
+  sns_1: Sensor,
+  aux_1: Auxiliary,
+  swc_low: usize,
+  dual_link: usize,
+  exo_state: usize,
+  aux_2: Auxiliary,
+  boost_time: String,
+  error_code: usize,
+  aux230: usize,
+  error_state: usize,
+  sn: String,
+  filter_pump: FilterPump,
+  #[serde(flatten)]
+  _unknown_fields: HashMap<String, serde_json::Value>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Equipment {
+  Robot(Robot),
+  #[serde(rename = "swc_0")]
+  Swc0(SaltWaterChlorinator)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct Timer {
+  start: String,
+  end: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct Schedule {
+  active: usize,
+  enabled: usize,
+  endpoint: String,
+  id: String,
+  name: String,
+  timer: Timer,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct Schedules {
+  programmed: usize,
+  supported: usize,
+  sch1: Schedule,
+  sch2: Schedule,
+  sch3: Schedule,
+  sch4: Schedule,
+  sch9: Schedule,
+  sch10: Schedule,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct State {
+  reported: serde_json::Value,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct Heating {
+  enabled: usize,
+  priority_enabled: usize,
+  sp: usize,
+  sp_max: usize,
+  sp_min: usize,
+  state: usize,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct ReportedState {
   dt: Option<String>,
   aws: serde_json::Value,
   ebox_data: Option<serde_json::Value>,
-  equipment: serde_json::Value,
+  equipment: Equipment,
   job_id: Option<String>,
   sn: Option<String>,
   vr: String,
   main: Option<serde_json::Value>,
   hmi: Option<serde_json::Value>,
-  schedules: Option<serde_json::Value>,
+  schedules: Option<Schedules>,
   state: Option<serde_json::Value>,
-  heating: Option<serde_json::Value>,
+  heating: Option<Heating>,
   debug: Option<serde_json::Value>,
   #[serde(flatten)]
   _unknown_fields: HashMap<String, serde_json::Value>,
